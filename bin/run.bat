8e7012c50ac8ca5233882eca99b70b043c29d359
ruihui-c-language <372491879@qq.com>
2026-04-07 18:02:34 +0800

    1

--- bin/run.bat
+++ bin/run.bat
@@ -4,11 +4,11 @@ echo [信息] 使用Jar命令运行Web工程。
 echo.
 
 cd %~dp0
-cd ../ruoyi-admin/target
+cd ../xingchen-admin/target
 
 set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -jar %JAVA_OPTS% ruoyi-admin.jar
+java -jar %JAVA_OPTS% xingchen-admin.jar
 
 cd bin
 pause
